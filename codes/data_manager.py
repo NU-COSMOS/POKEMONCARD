b77065010450af6d386ac30c960c52c4ead4c593
NU-COSMOS <nucosmos1234@gmail.com>
2021-08-16 12:12:20 +0900

    機能を追加

--- codes/data_manager.py
+++ codes/data_manager.py
@@ -11,13 +11,13 @@ class Checker:
     """
     def __init__(self):
         self.card_types = ["Monster", "Trainer", "Accessory", "Energy", "Field"]
+        self.types = ["炎", "水", "電気", "無", "闘", "悪", "鋼", "超", "草", "妖", "竜", "None"]
 
     def common(self, card):
         """
         共通項目
         """
         res = []
-        # カード名のチェック
         # カード名が文字列かどうかチェック
         res.append(type(card["name"]) == str)
 
@@ -29,27 +29,51 @@ class Checker:
 
         return all(res)
 
-    def monster(card):
+    def monster(self, card):
         """
         モンスター
         """
+        res = []
+        # 体力が10の倍数かつ0以下でないか
+        res.append(card["hp"] % 10 == 0 and card["hp"] <= 0)
 
-    def trainer(card):
+        # タイプに存在するものが入力されているか
+        for type in card["types"]:
+            res.append(type in self.types)
+
+        # タイプにNoneが含まれていないか
+        res.append("None" not in card["types"])
+
+        # 弱点属性に存在するものが入力されているか
+        for weak in card["weaks"]:
+            res.append(weak in self.types)
+
+        # 弱点属性にNoneが含まれている場合, Noneだけか
+        if "None" in card["weaks"]:
+            res.append(len(card["weaks"] == 1))
+
+        # 抵抗属性に存在するものが入力されているか
+        for resist in card["resists"]:
+            res.append(resist in self.types)
+        for escape in card["escape"]:
+            res.append(escape in self.types)
+
+    def trainer(self, card):
         """
         トレーナー
         """
 
-    def accessory(card):
+    def accessory(self, card):
         """
         持ち物
         """
 
-    def energy(card):
+    def energy(self, card):
         """
         エネルギーカード
         """
 
-    def field(card):
+    def field(self, card):
         """
         フィールドカード
         """
@@ -79,6 +103,25 @@ class Data:
             if checker.common(new_card):
                 break
             print("入力内容に誤りがあるのでもう一度やり直してください")
+
+        # 各カードタイプごとの情報を入力
+        while(1):
+            # モンスターカードの入力内容
+            if new_card["card_type"] == "Monster":
+                new_card["hp"] = int(input("モンスターの体力："))
+                print("属性一覧：{}".format(checker.types))
+                new_card["types"] = input("モンスターのタイプ(例：炎,水)：").split(",")
+                new_card["weaks"] = input("モンスターの弱点属性(例：炎,水)：").split(",")
+                new_card["resists"] = input("モンスターの抵抗属性(例：炎,水)：").split(",")
+                new_card["escape"] = input("モンスターが逃げるのに必要なエネルギー(例：炎,炎)：").split(",")
+                new_card["chara"] = input("特性名：")
+                new_card["skills"] = input("技名(例：なきごえ,たいあたり)：").split(",")
+                new_card["before"] = input("進化前(たね or ポケモン名)：")
+
+            # 入力内容をチェック
+            if checker.monster(new_card):
+                break
+            print("モンスターの入力内容に誤りがあるのでもう一度やり直してください")
 
     def delete():
         """
