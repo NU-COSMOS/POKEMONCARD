3ecac20a8b0b2e15ba060f3aca1b173f1d5259bf
NU-COSMOS <nucosmos1234@gmail.com>
2021-08-29 03:57:05 +0900

    バトルの流れの大枠を記述

--- codes/area.py
+++ codes/area.py
@@ -5,12 +5,13 @@
 
 
 class Area:
-    def __init__(self, deck):
-        self.deck = deck  # 山札
+    def __init__(self, player):
+        self.deck = player.deck  # 山札
+        self.player_name = player.name  # プレイヤー名
         self.hands = []  # 手札
         self.fight = []  # バトル場
         self.bench = []  # ベンチ
         self.trash = []  # トラッシュ
+        self.max_sides = player.deck.remain() // 10  # サイドにおける枚数
         self.sides = []  # サイド
-        self.max_sides = deck.remain() // 10  # サイドにおける枚数
-        self.studium = []  # スタジアムカード置き場
+        # self.studium = []  # スタジアムカード置き場
---
+++ codes/battle.py
@@ -0,0 +1,139 @@
+#-*- coding:utf-8 -*-
+"""
+試合する
+"""
+import pickle
+
+from player import Player
+from area import Area
+
+
+def load_deck(deck_name):
+    """
+    デッキデータの読み込み
+    """
+    with open(f"../{deck_name}.pkl", "r") as file:
+        deck = pickle.load(file)
+
+    return deck
+
+
+def create_players():
+    """
+    プレイヤーインスタンスの生成
+    """
+    players = []
+
+    for p in range(2):
+        name = input(f"プレイヤー{p+1}の名前を入力してください：")
+        deck = load_deck(input(f"{name}が使用するデッキ名を入力してください："))
+        players.append(Player(name, deck))
+
+    return players
+
+
+def set_area():
+    """
+    試合を開始するためにプレイゾーンを設定
+    """
+    # プレイヤーを用意
+    players = create_players()
+
+    areas = []
+    # プレイエリアインスタンスの生成
+    for player in players:
+        areas.append(Area(player))
+
+    areas = ready(areas)
+
+    return areas
+
+
+def ready(areas):
+    """
+    試合開始の準備を整える
+    """
+    # 手札を用意
+    # たねポケモンが含まれていなければ引き直し
+
+    # サイドを用意
+
+    return areas
+
+
+def check_end(areas):
+    """
+    試合が終了したかどうかを判定
+    """
+    return True
+
+
+def turn(areas, turn_cnt):
+    """
+    手番
+    """
+    print(f"{turn_cnt+1}ターン目")
+    print(f"{areas[turn_cnt%2].player_name}のターンです")
+
+    while(1):
+        act = int(input("行動を選択して下さい\n1:攻撃\n2:終了\n"))
+        if act == 1:
+            # 技を使用したらターン終了
+            break
+
+        elif act == 2:
+            # 技を使わずにターン終了
+            break
+
+    return areas
+
+
+def progress(areas):
+    """
+    各プレイヤーが交互に手番を行う
+    """
+    turn_cnt = 0
+    while(1):
+        areas = turn(areas, turn_cnt)
+
+        # 試合終了判定
+        if check_end(areas):
+            print("試合終了")
+            break
+
+        turn_cnt += 1
+
+    return areas
+
+
+def judge(areas):
+    """
+    勝敗の判定
+    """
+    winner = check_winner(areas)
+    print(winner, "の勝利!")
+
+
+def check_winner(areas):
+    """
+    勝者を調べる
+    """
+    for area in areas:
+        winner = area.name
+
+    return winner
+
+
+def main():
+    # 場を用意
+    areas = set_area()
+
+    # 勝負
+    areas = progress(areas)
+
+    # 勝敗判定
+    judge(areas)
+
+
+if __name__ == "__main__":
+    main()
