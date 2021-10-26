db0f3bcd7142d582ab6086becd0f5910210b69a7
mayumin <mayumin19960816@icloud.com>
2021-10-26 20:39:49 +0900

    書き間違え修正

--- codes/action.py
+++ codes/action.py
@@ -1,7 +1,7 @@
 #-*- codoing:utf-8 -*-
 
 from card import Monster
-from calculation import Calculation
+from skill_effect import Calculation
 from skill_effect import Target
 
 class Action:
@@ -65,17 +65,7 @@ class Action:
             areas[(turn_cnt+1)%2].battle[-1].change_status(block['status'])
 
         return areas
-
-
-#    @staticmethod
-#    def pokemon_check(areas, turn_cnt):
-#        """
-#        特殊状態のポケモンをチェック
-#        """
-#        for turn in range(2):
-#            if areas[(turn_cnt+turn)%2].battle[-1].status_effect(areas[(turn_cnt+turn)%2].player_name) == "毒":
-#                areas[(turn_cnt+turn)%2].battle[-1].change_cur_hp(10)     
-
+    
     @staticmethod
     def set_bench(areas, turn_cnt):
         """
